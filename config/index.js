df41150fce81a6ecf24b5bd83e3994f7c97f7f5d
vance Rodgers <vance.rodgers@gmail.com>
2017-09-13 11:59:59 -0700

    added ignore file

--- config/index.js
+++
@@ -1,8 +0,0 @@
-//In Real World these values would be encrypted and decrupted at the endpoint
-var configValues = require('./config');
-
-module.exports = {
-    getDBConnStr: configValues.uri,
-    allvalues: configValues
-
-}
